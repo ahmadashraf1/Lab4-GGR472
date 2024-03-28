487a9b9e23672a90a3daab6a3110572b808495b5
Ahmad Ashraf <ahmad.ashraf@mail.utoronto.ca>
2024-03-28 07:09:17 -0400

    data updated

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 let colgeojson;
 
 // Fetch GeoJSON from URL and store response
-fetch('https://raw.githubusercontent.com/ahmadashraf1/GGR472-Lab4/main/pedcyc_collision_06-21.geojson')
+fetch('https://raw.githubusercontent.com/ahmadashraf1/Lab4-GGR472/main/pedcyc_collision_06-21.geojson')
     .then(response => response.json())
     .then(response => {
         // console.log(response); // Check response in console
